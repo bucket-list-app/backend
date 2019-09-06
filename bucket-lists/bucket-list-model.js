eb30e3c3a843a9ec696aa20c24fd411be0359185
Pherpher089 <Christutor089@gmail.com>
2019-09-06 14:46:11 -0700

    Added routs for updating and removing a bucket list, updating and removing the bucket list items, and adding, updating and removing a photo

--- bucket-lists/bucket-list-model.js
+++ bucket-lists/bucket-list-model.js
@@ -1,61 +1,63 @@
-const db = require('../data/db-config');
+const db = require("../data/db-config");
 
 module.exports = {
-    add,
-    find,
-    findBy,
-    findById,
-    update,
-    remove,
-    addBucketListItem,
-    findBucketListItem,
-    findBucketListItemById,
-    updateBucketListItem,
-    removeBucketListItem,
-    addPhotos,
-    findPhotos,
-    findPhotoById,
-    updatePhoto,
-    removePhoto
-}
+	add,
+	find,
+	findBy,
+	findById,
+	update,
+	remove,
+	addBucketListItem,
+	findBucketListItem,
+	findBucketListItemById,
+	updateBucketListItem,
+	removeBucketListItem,
+	addPhoto,
+	findPhotos,
+	findPhotoById,
+	updatePhoto,
+	removePhoto,
+};
 
 ///////////////
 //bucket list//
 ///////////////
 
+//Returns all bucket lists
 function find() {
-    return db('BucketList')
+	return db("BucketList");
 }
-  
-function findBy(filter) {
-    return db('BucketList').where(filter);
-}
-  
-async function add(bucketList) {
-    const [id] = await db('BucketList').insert(bucketList);
 
-return findById(id);
+//Find and return bucket list based on the serch filter provided
+function findBy(filter) {
+	return db("BucketList").where(filter);
 }
-  
+
+async function add(bucketList) {
+	const [id] = await db("BucketList").insert(bucketList);
+
+	return findById(id);
+}
+
 function findById(id) {
-    return db('BucketList')
-        .where({ id })
-        .first();
+	return db("BucketList")
+		.where({ id })
+		.first();
 }
 
 async function update(changes, id) {
-    await db('BucketList')
-        .where({ id })
-        .update(changes);
+	await db("BucketList")
+		.where({ id })
+		.update(changes);
 
-    return findById(id);
+	return findById(id);
 }
 
 function remove(id) {
-    // returns removed count
-    return db('BucketList')
-      .where({ id })
-      .del();
+	// returns removed count
+	return db("BucketList")
+		.where({ id })
+		.del();
 }
 ///////////////////////////////
 
@@ -63,72 +65,70 @@ function remove(id) {
 //Bucket List Item//
 ////////////////////
 function findBucketListItem() {
-    return db('BucketListItem')
+	return db("BucketListItem");
 }
-  
-async function addBucketListItem(item) {
-    const [id] = await db('BucketListItem').insert(item);
 
-return findById(id);
+async function addBucketListItem(item) {
+	const [id] = await db("BucketListItem").insert(item);
+
+	return findById(id);
 }
-  
+
 function findBucketListItemById(id) {
-    return db('BucketListItem')
-        .where({ id })
-        .first();
+	return db("BucketListItem")
+		.where({ id })
+		.first();
 }
 
 async function updateBucketListItem(changes, id) {
-    await db('BucketListItem')
-        .where({ id })
-        .update(changes);
+	await db("BucketListItem")
+		.where({ id })
+		.update(changes);
 
-    return findById(id);
+	return findById(id);
 }
 
 function removeBucketListItem(id) {
-    // returns removed count
-    return db('BucketListItem')
-      .where({ id })
-      .del();
+	// returns removed count
+	return db("BucketListItem")
+		.where({ id })
+		.del();
 }
 /////////////////////////////
-
 
 //////////
 //Photo//
 /////////
 
+// Returns all photos
 function findPhotos() {
-    return db('Photos')
+	return db("Photos");
 }
-  
-  
-async function addPhotos(photo) {
-    const [id] = await db('Photos').insert(photo);
 
-return findById(id);
+async function addPhoto(photo) {
+	const [id] = await db("Photos").insert(photo);
+
+	return findById(id);
 }
-  
+
 function findPhotoById(id) {
-    return db('Photos')
-        .where({ id })
-        .first();
+	return db("Photos")
+		.where({ id })
+		.first();
 }
 
 async function updatePhoto(changes, id) {
-    await db('Photos')
-        .where({ id })
-        .update(changes);
+	await db("Photos")
+		.where({ id })
+		.update(changes);
 
-    return findById(id);
+	return findById(id);
 }
 
 function removePhoto(id) {
-    // returns removed count
-    return db('Photos')
-      .where({ id })
-      .del();
+	// returns removed count
+	return db("Photos")
+		.where({ id })
+		.del();
 }
 /////////////////////////////
-
--- bucket-lists/bucket-list-router.js
+++ bucket-lists/bucket-list-router.js
@@ -1,44 +1,183 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const BucketLists = require('./bucket-list-model');
+const BucketLists = require("./bucket-list-model");
 
-router.get('/', async (req, res) => {
-    try {
-        const bucketLists = await BucketLists.find();
-        res.json(bucketLists);
-    } catch (err) {
-        res.status(500).json({ message: 'Failed to get Bucket Lists' });
-    }
+//Gets all bucket lists
+router.get("/", async (req, res) => {
+	try {
+		const bucketLists = await BucketLists.find();
+		res.json(bucketLists);
+	} catch (err) {
+		res.status(500).json({ message: "Failed to get Bucket Lists" });
+	}
 });
 
-router.post('/', async (req, res) => {
-    const bucketListData = req.body;
-    try {
-        const bucketList = await BucketLists.add(bucketListData);
-        res.status(201).json(bucketList);
-    } catch (err) {
-        res.status(500).json({ message: "Failed to add Bucket List" });
-    }
+//adds a bucket list
+router.post("/", async (req, res) => {
+	const bucketListData = req.body;
+	try {
+		const bucketList = await BucketLists.add(bucketListData);
+		res.status(201).json(bucketList);
+	} catch (err) {
+		res.status(500).json({ message: "Failed to add Bucket List" });
+	}
 });
 
-router.get('/item', async (req, res) => {
-    try {
-        const bucketListItems = await BucketLists.findBucketListItem();
-        res.json(bucketListItems);
-    } catch (err) {
-        res.status(500).json({ message: 'Failed to get Bucket List Item' });
-    }
+//Updates bucket list
+//TODO route needs to be tested
+router.put("/:id", async (req, res) => {
+	const id = req.params.id;
+	const updatedlist = req.body;
+	if (!updatedlist) {
+		res.status(400).json({
+			message:
+				"Please provide the updated item in the body of the request and try again",
+		});
+	}
+
+	try {
+		const updated = await db.update(updatedlist, id);
+		res.status(201).json(updated);
+	} catch ({ message }) {
+		res.status(500).json(message);
+	}
 });
 
-router.post('/item', async (req, res) => {
-    const bucketListItemData = req.body;
-    try {
-        const bucketListItem = await BucketLists.addBucketListItem(bucketListItemData);
-        res.status(201).json(bucketListItem);
-    } catch (err) {
-        res.status(500).json({ message: "Failed to add Bucket List Item" });
-    }
+//Removes a bucket list item
+//TODO need to test this route
+router.delete("/:id", async (req, res) => {
+	const id = req.params.id;
+	try {
+		const deleted = await db.remove(id);
+		res.status(201).json(deleted);
+	} catch ({ message }) {
+		res.status(500).json(message);
+	}
 });
 
+//Returns all bucket list items
+router.get("/item", async (req, res) => {
+	try {
+		const bucketListItems = await BucketLists.findBucketListItem();
+		res.json(bucketListItems);
+	} catch (err) {
+		res.status(500).json({ message: "Failed to get Bucket List Item" });
+	}
+});
+
+//Adds a bucket list item to a bucket list
+router.post("/item", async (req, res) => {
+	const bucketListItemData = req.body;
+	try {
+		const bucketListItem = await BucketLists.addBucketListItem(
+			bucketListItemData
+		);
+		res.status(201).json(bucketListItem);
+	} catch (err) {
+		res.status(500).json({ message: "Failed to add Bucket List Item" });
+	}
+});
+
+// Updates a bucket list item
+//TODO need to test this route
+router.put("/item/:id", async (req, res) => {
+	const id = req.params.id;
+	const updatedItem = req.body;
+	if (!updatedItem) {
+		res.status(400).json({
+			message:
+				"Please provide the updated item in the body of the request and try again",
+		});
+	}
+
+	try {
+		const updated = await db.updateBucketListItem(updatedItem, id);
+		res.status(201).json(updated);
+	} catch ({ message }) {
+		res.status(500).json(message);
+	}
+});
+
+//Removes a bucket list item
+//TODO need to test this route
+router.delete("/item/:id", async (req, res) => {
+	const id = req.params.id;
+	try {
+		const deleted = await db.removeBucketListItem(id);
+		res.status(201).json(deleted);
+	} catch ({ message }) {
+		res.status(500).json(message);
+	}
+});
+
+// Pictures
+
+// Returns all findPhotos
+// TODO this route still needs to be tested
+router.get("/pictures", async (req, res) => {
+	try {
+		const photos = await db.findPhotos();
+		res.status(201).json(photos);
+	} catch ({ message }) {
+		res.status(500).json(message);
+	}
+});
+
+// Adds a photo to db
+// I think that we may need to pass a bucket list id into this one
+// TODO still needs to be tested.
+router.post("/pictures", async (req, res) => {
+	const photo = req.body.photo;
+
+	if (!photo) {
+		res.status(401).json({
+			message:
+				"please provide a path to the photo in the body of the request as photo: ",
+		});
+	}
+
+	try {
+		const added = await db.addPhotos(photo);
+		res.status(201).json(added);
+	} catch ({ message }) {
+		res.status(500).json(message);
+	}
+});
+
+// Updates a photo
+// TODO Need to set up the Photos table with an unique id so that we can target them directly.
+// TODO still needs to be tested
+router.put("/pictures/:id", async (req, res) => {
+	const id = req.params.id;
+	const newPhoto = req.body.photo;
+
+	if (!newPhoto) {
+		res
+			.status(422)
+			.json({
+				message:
+					"please provide a new photo to update with in the body of this request as {photo: newPhotoPath}",
+			});
+	}
+
+	try {
+		const updated = await db.updatePhoto(newPhoto, id);
+		res.status(201).json(updated);
+	} catch ({ message }) {
+		res.status(500).json(message);
+	}
+});
+
+// Removes the photo with {id}
+// TODO still needs to be
+router.delete("/pictures/:id", async (req, res) => {
+	const id = req.params.id;
+	try {
+		const removed = await db.removePhoto(id);
+		res.status(201).json(removed);
+	} catch ({ message }) {
+		res.status(500).json(message);
+	}
+});
 
 module.exports = router;
