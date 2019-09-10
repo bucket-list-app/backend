7ea3d91972fe60c01b20cc362615842b0fef20a1
Chaz Rowe <chazr_88@hotmail.com>
2019-09-10 00:04:39 -0400

    fixing issues

--- knexfile.js
+++ knexfile.js
@@ -16,7 +16,7 @@ module.exports = {
 	},
 	production: {
 		client: "pg",
-		connection: "process.env.DATABASE_URL",
+		connection: process.env.DATABASE_URL,
 		useNullAsDefault: true,
 		migrations: {
 			directory: "./data/migrations",
